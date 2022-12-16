80595492f7cd878a3932ec57f4a50ee7b4a29d26
adwaitjoshi009 <adwaitjoshi009@gmail.com>
2022-12-16 23:50:37 +0530

    added a component and used ngFor to render it

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = []
+  recipes: Recipe[] = [
+    new Recipe('Biryani', 'Spicy and tasty', 'https://live.staticflickr.com/3359/3643374206_bae9925038_b.jpg')
+  ]
 
   constructor() { }
 
